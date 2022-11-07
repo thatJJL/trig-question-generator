772d9f8ab1172848158f9dca8aa8c5c29cdf2674
JJL <94117412+thatJJL@users.noreply.github.com>
2022-11-07 12:31:24 +0000

    Add files via upload

--- triangle-gen.py
+++ triangle-gen.py
@@ -1,4 +1,4 @@
-#triangle-gen1
+#triangle-gen2
 
 #https://www.omnicalculator.com/math/trigonometry
 #Trig from/using area - TODO 11:14am 7/11/2022 i1
@@ -7,16 +7,11 @@
 
 import math
 
-#def generate_triangle(config):
-    #config - string of 3 characters, either L or A representing and storing the order of data of length or angle.
-#def generate_triangle(isLAA, data):
-#def generate_triangle(isLAA, lengths, angles):
-def solve_trig_triangle(isLAA, lengths, angles):
+def solve_trig_triangle(is_LAA, lengths, angles):
     #isLAA - boolean.
     #True: treating triangle as length-angle-angle
     #False: treating triangle as length-length-angle
 
-    #data - discontinued
     #lengths, angles - seperate arrays
     '''
     Types of triangle questions
@@ -24,16 +19,49 @@ def solve_trig_triangle(isLAA, lengths, angles):
     Length, length, angle
     '''
 
+    #variables for test validation
+    do_tests = False
+    final_length_a = None
+    final_length_b = None
+    final_length_c = None
+    final_angle_A = None
+    final_angle_B = None
+    final_angle_C = None
+
+    #Data validation
+    if isinstance(lengths, int): #Repack integers into arrays
+        lengths = [lengths]
+    if isinstance(angles, int):
+        angles = [angles]
+
     #sin A / a = sin B / b = sin C / c
 
     #Data assignment by configuration
-    if isLAA:
+    if is_LAA:
         given_length_a = lengths[0] #Known/Given values
         given_angle_A = angles[0]
         given_angle_B = angles[1]
 
-        solved_angle_b = ( given_length_a / math.sin( given_angle_A ) ) * math.sin(given_angle_B)
-        print(solved_angle_b)
+        print(f"given_length_a: {given_length_a}")
+        print(f"given_angle_A: {given_angle_A}")
+        print(f"given_angle_B: {given_angle_B}")
+        
+        #solved_angle_b = ( given_length_a / math.sin( given_angle_A ) ) * math.sin(given_angle_B)
+        #solved_angle_b = ( given_length_a  * math.sin(given_angle_B) ) / math.sin( given_angle_A )
+        #print(f"solved_angle_b: {solved_angle_b}")
+        
+        #step1 = given_length_a * math.sin(given_angle_B)
+        step1 = math.sin(given_angle_B)
+        print(f"step1: {step1}")
+
+        #https://www.symbolab.com/solver/equation-calculator/%20%5Cfrac%7Bsin%5Cleft(A%5Cright)%7D%7Ba%7D%3D%5Cfrac%7Bsin%5Cleft(B%5Cright)%7D%7Bx%7D?or=input
+        #https://www.symbolab.com/solver/equation-calculator/%20%5Cfrac%7Bsin%5Cleft(90%5Cright)%7D%7B10%7D%3D%5Cfrac%7Bsin%5Cleft(60%5Cright)%7D%7Bx%7D?or=input
+
+        step2 = given_length_a * math.sin(given_angle_B)
+        print(f"step2: {step2}")
+
+
+        
         
     else: #LLA
         given_length_a = lengths[0]
@@ -46,8 +74,11 @@ def solve_trig_triangle(isLAA, lengths, angles):
 
 def main():
     print("main function")
-    generate_triangle(True, [10], [90, 60] )
-    solve_trig_triangle(True, [10], [90, 60] )
+    #generate_triangle(True, 10, [90, 60] )
+    solve_trig_triangle(True, 10, [90, 60] )
+    print(  "Correct answer: " + str( 5 * ( 3**(1/2) ) )  )
+    #solve_trig_triangle(True, [10], [60, 90] )
+    
 
 if __name__ == "__main__":
     main()
